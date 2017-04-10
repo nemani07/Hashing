7bf1382a82b99228a69211daff03f94980825fa1
RoyFossil <bamfpita@gmail.com>
2017-04-10 18:12:33 -0400

    Splitting (WIP)

--- LinearlyHashedFile.py
+++ LinearlyHashedFile.py
@@ -8,6 +8,7 @@ class LinearlyHashedFile:
 		self.m = 1
 		self.n = 0
 		self.file = fileLoc
+		self.overflow = fileLoc + "_overflow"
 		self.blockSize = blockSize
 		# record size supplied by user should include the hash field size
 		# 1 is added for the deletion marker
@@ -17,11 +18,11 @@ class LinearlyHashedFile:
 		self.bfr = math.floor((blockSize-self.blockPointerSize)/self.recordSize)
 		# truncates the file
 		with open(self.file, 'wb') as f:
-			f.write(b"""This is a file header
-			here is some information
-			this is only two blocks in size at maximum""")
+			f.write(b"This is a shorter less ridicculous file header")
+			f.seek(self.blockSize*3 - self.blockPointerSize)
+			f.write((0).to_bytes(fieldSize, byteorder='big'))
 		# create overflow file
-		open(self.file + '_overflow', 'wb').close()
+		open(self.overflow, 'wb').close()
 	
 	def h1(self, value):
 		return value % self.m
@@ -35,9 +36,8 @@ class LinearlyHashedFile:
 		# check to see if the secondary hash function needs to be used
 		if bucket < self.n:
 			bucket = self.h2(value)
-		# encode the record to be inserted
+		# format the record to be inserted
 		formattedRecord = Record.new(self.recordSize, self.fieldSize, value, record)
-		print(formattedRecord)
 		# open the file as binary read and write
 		with open(self.file, 'r+b', buffering=self.blockSize) as f:
 			# navigate to the appropriate bucket
@@ -45,7 +45,7 @@ class LinearlyHashedFile:
 			f.seek(self.blockSize*(bucket+2))
 			# check to see if data exists in this bucket
 			theBlock = self.makeBlock(f.read(self.blockSize))
-			space = theBlock.hasSpace();
+			space = theBlock.hasSpace()
 			if space>=0:
 				# spot was open, move pointer back
 				f.seek(self.blockSize*(bucket+2) + self.recordSize*space)
@@ -57,9 +57,23 @@ class LinearlyHashedFile:
 				print("need a split yoooooo")
 				
 				# here is where we need to put the data into an overflow bucket
-				# idk how to do that though.
-				# so just ignore it for now.....
-				
+				with open(self.overflow, 'r+b') as overflow:
+					print(theBlock.getPointer())
+					# need to ignore the 0 pointer because that'll be null pointer
+					# but we don't want to waste the first block in overflow
+					# so we could subtract one? or maybe use first block for info
+					pointer = theBlock.getPointer() - 1
+					# only if the pointer points to something, meaning that this block already has an overflow bucket assigned to it
+					if pointer >= 0:
+						overflow.seek(self.blockSize*(pointer))
+						overflowBlock = self.makeBlock(overflow.read(self.blockSize))
+						overflowSpace = overflowBlock.hasSpace()
+						if overflowSpace>=0:
+							overflow.seek(self.blockSize*(
+						
+					# else the bucket doesn't already have an overflow bucket assigned to it. so we must make one.
+					else:
+					
 				# navigate to the bucket to be split
 				f.seek(self.blockSize*(self.n+2))
 				
--- hashing.py
+++ hashing.py
@@ -1,11 +1,12 @@
 from LinearlyHashedFile import *
 
-file = LinearlyHashedFile(512, 100, 10, 'C:/RF/test1')
-file2 = LinearlyHashedFile(256, 50, 10, 'C:/RF/test2')
+file = LinearlyHashedFile(256, 100, 10, 'C:/RF/test1')
 
 
-file.insert(53, "this is a string in file 1")
-file2.insert(10, "this is a string in file 2")
+file.insert(51, "first record")
+file.insert(52, "second record")
+file.insert(53, "third record")
+
 
 
 # lst = [12,34,200,255]
